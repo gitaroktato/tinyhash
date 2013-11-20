29b6f5c91fc6ab7798a28d67f93428ba23e922d1
popnam <maoreszt@gmail.com>
2013-11-20 22:41:38 +0100

    Added simple utf8 iterator which swallows the inappropriate bite ranges.

---
+++ TinyHashApplication/src/com/yourcompanyhere/tinyhash/SimpleUtf8Iterator.java
@@ -0,0 +1,57 @@
+package com.yourcompanyhere.tinyhash;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.MalformedInputException;
+
+public class SimpleUtf8Iterator {
+	
+	private static final int MAX_STRING_SIZE = 6;
+	private static final int CHAR_SIZE = 4;
+	private static final int MAX_SIZE_IN_BYTES = MAX_STRING_SIZE * CHAR_SIZE;
+	private int currentPosition = MAX_SIZE_IN_BYTES - 1;
+	private byte[] currentText = new byte[MAX_SIZE_IN_BYTES];
+	private boolean hasNext = true;
+	private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
+	private CharsetDecoder decoder = UTF8_CHARSET.newDecoder();;
+	
+	public String next() throws CharacterCodingException {
+		if (!hasNext) {
+			return null;
+		}
+		String result = null;
+		ByteBuffer data;
+		while (result == null) {
+			try {
+				 data = ByteBuffer.wrap(currentText, currentPosition, MAX_SIZE_IN_BYTES - currentPosition);
+				result = decoder.decode(data).toString();
+			} catch (MalformedInputException ex) {
+				// We try to increment until decoder finds a valid value.
+			}
+			increment();
+		}
+		return result;
+	}
+	
+	private void increment() {
+		if (currentText[MAX_SIZE_IN_BYTES - 1] != -1) {
+			currentText[MAX_SIZE_IN_BYTES - 1]++;
+		} else {
+			int i = MAX_SIZE_IN_BYTES - 1;
+			while (currentText[i] == -1) {
+				currentText[i] = 0;
+				if (--i < 0) {
+					hasNext = false;
+				} else {
+					currentText[i]++;
+				}
+			}
+			if (hasNext) {
+				currentPosition = i; 
+			}
+		}
+	}
+
+}
---
+++ TinyHashTest/src/com/yourcompanyhere/tinyhash/test/SimpleUtf8IteratorTest.java
@@ -0,0 +1,79 @@
+package com.yourcompanyhere.tinyhash.test;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import com.yourcompanyhere.tinyhash.SimpleUtf8Iterator;
+
+public class SimpleUtf8IteratorTest extends TestCase {
+	
+	public void testIterationWithFirstCharacter() throws CharacterCodingException {
+		// 0x21 is the character !.
+		SimpleUtf8Iterator it = new SimpleUtf8Iterator();
+		String value = "";
+		for(int iterations = 0; iterations != 0x22; iterations++) {
+			value = it.next();
+		}
+		Assert.assertEquals("!", value);
+	}
+	
+	public void testIterationWithOtherChar() throws CharacterCodingException {
+		// 0x40 is the character @.
+		SimpleUtf8Iterator it = new SimpleUtf8Iterator();
+		String value = "";
+		for(int iterations = 0; iterations != 0x41; iterations++) {
+			value = it.next();
+		}
+		Assert.assertEquals("@", value);
+	}
+	
+	public void testIterationWithNotAscii() throws CharacterCodingException {
+		SimpleUtf8Iterator it = new SimpleUtf8Iterator();
+		String expected = "é";
+		String value = "";
+		int iterations = 0;
+		for(; iterations != 65535 && !expected.equals(value); iterations++) {
+			value = it.next();
+		}
+		Assert.assertEquals(expected, value);
+	}
+	
+	public void testIterationWithTwoLetters() throws CharacterCodingException {
+		SimpleUtf8Iterator it = new SimpleUtf8Iterator();
+		String expected = "da";
+		String value = "";
+		int iterations = 0;
+		for(; iterations != 65535 && !expected.equals(value); iterations++) {
+			value = it.next();
+		}
+		Assert.assertEquals(expected, value);
+	}
+	
+	public void testIterationWithSimpleWords() throws Exception {
+		SimpleUtf8Iterator it = new SimpleUtf8Iterator();
+		String expected = "oé";
+		byte[] expectedValue = expected.getBytes("UTF8");
+		String value = "";
+		int iterations = 0;
+		for(; iterations != Integer.MAX_VALUE && !expected.equals(value); iterations++) {
+			value = it.next();
+		}
+		Assert.assertEquals(expected, value);
+	}
+	
+	public void testImplementation() throws Exception {
+		String expected = "oé";
+		byte[] gotValue = expected.getBytes("UTF8");
+		byte[] expectedValue = {0, 0, 111, -23};
+		ByteBuffer data = ByteBuffer.wrap(expectedValue, 2, 2);
+		String result = Charset.forName("UTF-8").decode(data).toString();
+		String result2 = new String(gotValue, "UTF8");
+		Assert.assertEquals(expected, result2);
+		Assert.assertEquals(expected, result);
+	}
+}
