dc201d071413182ed37bc9f9a9032c6a012ccef8
oresztesz <maoreszt@gmail.com>
2013-11-20 14:28:14 +0100

    Added Utf8Iterator with 2 byte range from one character to two characters.

--- TinyHashApplication/src/com/yourcompanyhere/tinyhash/Utf8Iterator.java
+++ TinyHashApplication/src/com/yourcompanyhere/tinyhash/Utf8Iterator.java
@@ -4,45 +4,143 @@ import java.nio.ByteBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
-
+/**
+ * http://en.wikipedia.org/wiki/UTF-8
+ * @author oresztesz
+ */
 public class Utf8Iterator {
 	
 	private static final int MAX_STRING_SIZE = 255;
-	private static final int CHAR_SIZE = 4;
-	private static final int MAX_SIZE_IN_BYTES = MAX_STRING_SIZE * CHAR_SIZE;
-	private int currentPosition = MAX_SIZE_IN_BYTES - 1;
+	// Can be various according to size (1-6).
+	private static final int UTF8_BYTE_SIZE = 2;
+	private static final int MAX_SIZE_IN_BYTES = MAX_STRING_SIZE * UTF8_BYTE_SIZE;
+	private int lastByteOfCurrentChar = MAX_SIZE_IN_BYTES - 1;
 	private byte[] currentText = new byte[MAX_SIZE_IN_BYTES];
 	private boolean hasNext = true;
 	private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
+	
+	/*
+	 * TODO change API maybe:
+	 * - next
+	 * - getString
+	 * - getBytes
+	 */
 	
 	public String next() throws CharacterCodingException {
 		if (!hasNext) {
 			return null;
 		}
-		ByteBuffer data = ByteBuffer.wrap(currentText, currentPosition, MAX_SIZE_IN_BYTES - currentPosition);
+		// Getting the first position.
+		int firstPos = lastByteOfCurrentChar - getCurrentCharBytes(lastByteOfCurrentChar) + 1;
+		ByteBuffer data = ByteBuffer.wrap(currentText, firstPos, MAX_SIZE_IN_BYTES - firstPos);
 		CharsetDecoder decoder = UTF8_CHARSET.newDecoder();
 		String result = decoder.decode(data).toString();
-		increment();
+		increment(getLastIndexOfBuffer());
 		return result;
 	}
 	
-	private void increment() {
-		if (currentText[currentPosition] < 0xFF) {
-			currentText[currentPosition]++;
+	// TODO hasMoreInBuffer AND JUnit tests.
+	 
+	
+	int getLastIndexOfBuffer() {
+		return currentText.length - 1;
+	}
+	
+	private void increment(int pos) {
+		if (!isMaxAtPos(pos)) {
+			incrementCurrentByte(pos);
 		} else {
-			int i = currentPosition;
-			while (currentText[i] == 0xFF) {
-				currentText[i] = 0;
-				if (--i < 0) {
-					hasNext = false;
-				} else {
-					currentText[i]++;
-				}
-			}
-			if (hasNext) {
-				currentPosition = i; 
+			int charLastPos = pos;
+			while (isMaxAtPos(charLastPos)) {
+				int currentCharBytes = getCurrentCharBytes(charLastPos);
+				// Just modify this byte range [currentCharBytes]
+				handleByteRangeOverflow(charLastPos, currentCharBytes);
+				// Step char size. Bite size may change on byte range overflow TODO optimize?
+				charLastPos -= getCurrentCharBytes(charLastPos);
 			}
 		}
 	}
 
+	private void handleByteRangeOverflow(int charLastPos, int currentCharBytes) {
+		// Check if we can increment at any position before the last byte of current char:
+		for (int j = 1; j < currentCharBytes; j++) {
+			if (!isMaxAtPos(j)) {
+				incrementCurrentByte(charLastPos - j);
+				// Zero out all other bytes.
+				for (int k = 0; k < j; k++) {
+					currentText[charLastPos - k] = (byte) 0x80;
+				}
+				return;
+			}
+		}
+		// If loop ended without any modification, check if we should zero all bytes.
+		if (currentCharBytes == UTF8_BYTE_SIZE) {
+			// Zero out bytes according to current char size.
+			zeroOutAllBytes(charLastPos, currentCharBytes);
+			// Increment next character.
+			lastByteOfCurrentChar = charLastPos - currentCharBytes; 
+			increment(charLastPos - currentCharBytes);
+		} else {
+			// We should increment current char bytes by one.
+			// TODO temp code write to all the byte sizes. TODO why not C0???
+			currentText[charLastPos - currentCharBytes] = (byte) 0xC2;
+			currentText[charLastPos] = (byte) 0x80;
+		}
+	}
+
+	private void zeroOutAllBytes(int charLastPos, int currentCharBytes) {
+		for (int j = 0; j < currentCharBytes; j++) {
+			currentText[charLastPos - j] = 0; 
+		}
+	}
+	
+	private void incrementCurrentByte(int pos) {
+		if (isOneByted(pos)) {
+			currentText[pos]++;
+		} else if (isMultiBytedAndNotFirst(pos)) {
+			currentText[pos]++;
+		} else {
+			currentText[pos]++;
+		}
+	}
+	
+	private int getCurrentCharBytes(int pos) {
+		int bytesInSequence = 1;
+		while ((currentText[pos] & 0xC0) == 0x80) {
+			bytesInSequence++;
+			pos--;
+		}
+		return bytesInSequence;
+	}
+	/**
+	 * 1 byte representation is in 0xxx xxxx
+	 * @param pos
+	 * @return
+	 */
+	private boolean isOneByted(int pos) {
+		return (currentText[pos] & 0x80) == 0x00;
+	}
+	
+	/**
+	 * More than 1 byte representation, not first element. Format 10xx xxxx
+	 * @param pos
+	 * @return
+	 */
+	private boolean isMultiBytedAndNotFirst(int pos) {
+		return (currentText[pos] & 0xC0) == 0x80;
+	}
+	
+	private boolean isMaxAtPos(int pos) {
+		if (isOneByted(pos)) {
+			return currentText[pos] == 0x7F;
+		} else if (isMultiBytedAndNotFirst(pos)) {
+			return (currentText[pos] & 0x3F) == 0x3F;
+		} else if ((currentText[pos] & 0xE0) == 0xC0) {
+			// 2 byte representation, first element. Format 110x xxxx
+			return (currentText[pos] & 0x1F) == 0x1F;
+		} else {
+			// TODO ...
+			return true;
+		}
+	}
 }
--- TinyHashTest/src/com/yourcompanyhere/tinyhash/test/Utf8IteratorTest.java
+++ TinyHashTest/src/com/yourcompanyhere/tinyhash/test/Utf8IteratorTest.java
@@ -1,6 +1,8 @@
 package com.yourcompanyhere.tinyhash.test;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.CharacterCodingException;
+import java.nio.charset.MalformedInputException;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
@@ -9,13 +11,56 @@ import com.yourcompanyhere.tinyhash.Utf8Iterator;
 
 public class Utf8IteratorTest extends TestCase {
 	
-	public void testIteration() throws CharacterCodingException {
-		// 0x21 is the character !.
+	
+	public void testIterationWithAscii() throws CharacterCodingException, UnsupportedEncodingException {
 		Utf8Iterator it = new Utf8Iterator();
 		String value = "";
-		for(int iterations = 0; iterations != 0x22; iterations++) {
+		
+		for(int iterations = 0; iterations != 0x0040 + 1; iterations++) {
 			value = it.next();
 		}
-		Assert.assertEquals("!", value);
+		Assert.assertEquals("@", value);
+	}
+	
+	public void testIterationWithTwoAscii() throws CharacterCodingException, UnsupportedEncodingException {
+		Utf8Iterator it = new Utf8Iterator();
+		String value = "";
+		String expected = "da";
+		int iterations = 0;
+		while(!value.equals(expected) && iterations < 16000) {
+			value = it.next();
+			iterations++;
+		}
+		Assert.assertEquals("Got wrong value after end of iteration", expected, value);
+	}
+	
+	public void testIterationWithOneCharOverAscii() throws CharacterCodingException {
+		Utf8Iterator it = new Utf8Iterator();
+		String value = "";
+		String expected = "ú";
+		int iterations = 0;
+		try {
+			while(!value.equals(expected) && iterations < 128000) {
+				value = it.next();
+				iterations++;
+			}
+		} catch (CharacterCodingException ex) {
+			throw new RuntimeException("" + iterations, ex);
+		}
+		Assert.assertEquals(expected, value);
+	}
+	
+	public void testIterationWithOneChar2() throws CharacterCodingException {
+		Utf8Iterator it = new Utf8Iterator();
+		String value = "";
+		int iterations = 0;
+		try {
+			for(; iterations != 0x019F + 1; iterations++) {
+				value = it.next();
+			}
+		} catch(MalformedInputException ex) {
+			throw new RuntimeException("Exception at: " + iterations, ex);
+		}
+		Assert.assertEquals("\u019F", value);
 	}
 }
